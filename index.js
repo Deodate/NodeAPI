48735ca08feca08ff581dcd85da0f396f8dc37c2
Deodate <ajadeo04@gmail.com>
2024-08-11 23:06:46 +0200

    Progress

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
 const express = require('express');
 const app = express();
-const Pool = require('pg').Pool;
-const Path = require('path');
-const ejs = require('ejs');
-const { release } = require('os');
-const PORT = 3000;
-require('dotenv').config();
+const path = require('path'); 
+const PORT = 3010;
 
-const pool = new Pool({
-    user: process.env.USER_NAME,
-    host: process.env.HOST_NAME,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    dialect: process.env.DIALECT,
-    port: process.env.PORT_NUMBER
-})
 
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Error in connection');
-    }
-    client.query('SELECT NOW()', (err, result) => {
-        release();
-        if (err) {
-            return console.error('Error executing query');
-        }
-        console.log("Connected to database:")
-    });
+// Set up EJS view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Serve a basic message to check if the server is running
+app.get('/', (req, res) => {
+    console.log('Received request for /');
+    res.render('index'); // Ensure the 'index.ejs' file exists in the 'views' directory
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 });
+
+
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const Pool = require('pg').Pool;
+// const Path = require('path');
+// const ejs = require('ejs');
+// const PORT = 3010;
+// require('dotenv').config();
+
+// const pool = new Pool({
+//     user: process.env.USER_NAME,
+//     host: process.env.HOST_NAME,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     dialect: process.env.DIALECT,
+//     port: process.env.PORT_NUMBER
+// })
+
+// pool.connect((err, client, release) => {
+//     if (err) {
+//         return console.error('Error in connection');
+//     }
+//     client.query('SELECT NOW()', (err, result) => {
+//         release();
+//         if (err) {
+//             return console.error('Error executing query');
+//         }
+//         console.log("Connected to database:")
+//     });
+// });
+
+// app.set('views', path.join(__dirname, 'views')); 
+// app.set('view engine', 'ejs')
+// app.use('/static', express.static('static'))
+
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.json)
+
+// app.get('/', async(req, res) => {
+//     res.render('index');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server started at port ${PORT}`);
+// });
