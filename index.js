7f5f98c0fc322dbc806331b767d264e77e1cb2dd
Deodate <ajadeo04@gmail.com>
2024-08-11 21:51:49 +0200

    Progress

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ const ejs = require('ejs');
 const PORT = 3000;
 require('dotenv').config();
 
+const pool = new Pool({
+    user: process.env.USER_NAME,
+    host: process.env.HOST_NAME,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DIALECT,
+    port: process.env.PORT_NUMBER
+})
+
 app.listen(PORT, ()=>{console.log('Server strat at port ${PORT}')})
