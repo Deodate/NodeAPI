88a8bf52f038fa8e533413f77aef608633e0675b
Deodate <ajadeo04@gmail.com>
2024-08-11 22:01:51 +0200

    Connection to DB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const Pool = require('pg').Pool;
 const Path = require('path');
 const ejs = require('ejs');
+const { release } = require('os');
 const PORT = 3000;
 require('dotenv').config();
 
@@ -15,4 +16,19 @@ const pool = new Pool({
     port: process.env.PORT_NUMBER
 })
 
-app.listen(PORT, ()=>{console.log('Server strat at port ${PORT}')})
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error in connection');
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release();
+        if (err) {
+            return console.error('Error executing query');
+        }
+        console.log("Connected to database:")
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
